620552b151907c8f250e47a4d995b86c3ab91802
AliPi025 <94445337+AliPi025@users.noreply.github.com>
2025-02-25 15:46:32 +0100

    Add files via upload

---
+++ Server_Folder/sumCPU.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 1) {
+        printf("Usage: %s num1 num2 [...]\n", argv[0]);
+        return -1;
+    }
+    int sum = 0;
+    
+    for (int i = 1; i < argc; i++) {
+        sum += atoi(argv[i]);
+    }
+    printf("Sum: %d\n", sum);
+    
+    return sum;
+}
